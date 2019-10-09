4bb505a9e2d0ebe38dc42c20ed7d52bf4188d882
azubdavidson <bruce.davidson@nucleusfinancial.com>
2019-10-09 16:58:59 +0100

    first commit

---
+++ gatsby-node.js
@@ -0,0 +1,48 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const path = require('path');
+
+exports.createPages = async ({actions, graphql}) => {
+  const{createPage} = actions;
+  const articles = await graphql(`
+    {
+      allNodeArticle {
+      nodes {
+        id
+        title
+        body {
+          processed
+        }
+        field_image {
+          alt
+        }
+        relationships {
+          field_image {
+            localFile {
+              publicURL
+            }
+          }
+        }
+        path {
+          alias
+        }
+      }
+    }
+  }
+  `);
+  articles.data.allNodeArticle.nodes.map(articleData =>
+    createPage({
+      path: articleData.path.alias,
+      component: path.resolve(`src/templates/article.js`),
+      context: {
+        ArticleId: articleData.id,
+      },
+    })
+  );
+}
